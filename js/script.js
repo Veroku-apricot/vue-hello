2a5f835f633b98f8d5323cc0b3a3bc26e2a09df9
Veronika <veronikakuchtova95@gmail.com>
2020-11-16 17:38:40 +0100

    Bonus 2

--- js/script.js
+++ js/script.js
@@ -5,6 +5,21 @@ var app = new Vue({
   el: '#root',
   data: {
     message: 'Hello, this is one of my favourite Pokémon!',
-    image: 'img/Togekiss.gif' //Bonus 1: Add an img
+    image: 'img/Togepi.gif', //Bonus 1: Add an img
+    evo: 'Evolve!',
+    restart: 'Restart!'
+  },
+  methods: {
+    changeImg: function() {
+      if (this.image === 'img/Togepi.gif'){
+        this.image = 'img/Togetic.gif'
+      }else if (this.image === 'img/Togetic.gif'){
+        this.image = 'img/Togekiss.gif';
+        this.evo = 'Restart!'
+      }else if (this.image === 'img/Togekiss.gif'){
+        this.image = 'img/Togepi.gif';
+        this.evo = 'Evolve!'
+      }
+    }
   }
 });
