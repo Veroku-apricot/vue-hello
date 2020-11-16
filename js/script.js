0f929638a9ef45e79e0439aefdd8eaf4601afa8e
Veronika <veronikakuchtova95@gmail.com>
2020-11-16 15:51:54 +0100

    Bonus 1

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
-//Bonus: Aggiungere un’immagine presa anch’essa da un data Altro possibile Bonus: gestire una funzione con un evento nel modo Vue.
+// Altro possibile Bonus: gestire una funzione con un evento nel modo Vue.
 
 // Print a message on h1, using data
 var app = new Vue({
   el: '#root',
   data: {
-    message: 'Hello, this is one of my favourite Pokémon!'
+    message: 'Hello, this is one of my favourite Pokémon!',
+    image: 'img/Togekiss.gif' //Bonus 1: Add an img
   }
 });
